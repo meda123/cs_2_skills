8590f8c45f697fe5874f0c426ae9edf2a8cb8866
meda123 <mrt62@cornell.edu>
2017-04-23 14:37:57 -0700

    discussion questions almost complete.

---
+++ discussion_qs.py
@@ -0,0 +1,65 @@
+
+# Part One: Recursion 
+
+""" 
+1. In your own words, what is recursion?
+Recursion is a programming technique where a function calls itself, it can call
+itself once or more inside the function body. 
+
+2. Why is it necessary to have a base case?
+A base case is necessary because it is how we signal to the function that we're
+done recursing. In other words, without a base case, you'll have infinite recursion.
+
+"""
+
+# Graphs 
+
+"""
+1. What is a graph?
+A graph is a data structure, where a node is a basic unit of a graph, and an edge 
+connects nodes. A graph can de directed or un-directed. 
+
+
+2. How is a graph different from a tree?
+Unlike a tree, graphs contain loops (also known as cycles)
+
+
+3.Give an example of something that would be good to model with a graph.
+A food chain depicting who eats whom lends itself really well to be modeled by 
+a directed graph.
+
+"""
+
+# Performance of Different Data Structures
+
+"""
+Fill in the runtimes for the following actions for the table below:
+
+Data Structure                  Index   Search  Add-R   Add-L   Pop-L   Pop-R
+Python List (Array)             O(1)    O(n)    O(1)             
+Linked List                      
+Doubly-Linked List                       
+Queue (as Array)                X   X                   X                  X
+Queue (as LL or DLL)            X   X                   X                  X
+Stack (as Array, LL, or DLL)    X   X                   X       X    
+Deque (as DLL)                  X   X                
+
+
+
+Data Structure          Get     Add     Delete  Iterate     Memory
+Dictionary (Hash Map)   O(1)    O(1)    O(1)    O(n)        medium
+Set (Hash Map)                   
+Binary Search Tree                   
+Tree
+
+"""
+
+# Sorting 
+"""
+
+1. Describe in words how the Bubble Sort algorithm works.
+2. Describe in words how the Merge Sort algorithm works.
+3. Describe in words how the Quick Sort algorithm works
+
+""" 
+
